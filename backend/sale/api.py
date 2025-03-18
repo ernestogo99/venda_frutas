67dfdc7b7cd5c2455b5250054c4c750afa75fc09
ernesto <ernestodalva@gmail.com>
2025-03-18 18:12:46 -0300

    operação de venda finalizada

--- backend/sale/api.py
+++ backend/sale/api.py
@@ -1,7 +1,7 @@
 from ninja import Router
 from .models import Venda
 from fruits.models import Frutas
-from .schemas import ModelVendaSchema
+from .schemas import ModelVendaSchema,VendaSchema
 from fruits.api import check_permission,auth
 from django.shortcuts import get_object_or_404
 from decimal import Decimal
@@ -10,30 +10,33 @@ venda_router=Router()
 
 
 @venda_router.post('/',response={200:ModelVendaSchema,400:dict,401:dict,403:dict},auth=auth)
-def sell_fruit(request,id,quantidade:int,desconto=0):
-    tem_permissao,erro=check_permission(request,'cadastrar_fruta')
+def sell_fruit(request,body:VendaSchema):
+    tem_permissao,erro=check_permission(request,'realizar_venda')
     if not tem_permissao:
         return erro['status'], {'error': erro['error']}
-    fruta=get_object_or_404(Frutas,id=id)
-    if fruta.quantidade_disponivel< quantidade:
+ 
+    fruta=get_object_or_404(Frutas,id=body.id)
+ 
+    if fruta.quantidade_disponivel< body.quantidade:
         return {"error": "Quantidade insuficiente."}, 400
+  
     
     descontos_validos = [0, 5, 10, 15, 20, 25]
-    if desconto not in descontos_validos:
+    if body.desconto not in descontos_validos:
         return 400, {"error": f"Desconto inválido. Use: {descontos_validos}"}
+
     
-    valor_total=fruta.valor_venda * quantidade
-    desconto_decimal=Decimal(desconto)
+    valor_total=fruta.valor_venda * body.quantidade
+    desconto_decimal=Decimal(body.desconto)
     valor_com_desconto = valor_total * (Decimal(1) - desconto_decimal / Decimal(100))
     venda=Venda.objects.create(
         fruta=fruta,
-        quantidade=quantidade,
+        quantidade=body.quantidade,
         total=valor_com_desconto,
-        desconto=desconto,
-        vendedor=request.user
+        desconto=body.desconto,
+        vendedor=request.auth
     )
-    
-    fruta.quantidade_disponivel-=quantidade
+    fruta.quantidade_disponivel-=body.quantidade
     fruta.save()
     return venda
     
--- backend/sale/schemas.py
+++ backend/sale/schemas.py
@@ -1,12 +1,17 @@
-from ninja import ModelSchema
+from ninja import ModelSchema,Schema
 from .models import Venda
-
+from pydantic import Field
 class ModelVendaSchema(ModelSchema):
     class Meta:
         model=Venda
         fields='__all__'
         
         
+ 
+class VendaSchema(Schema):
+    id:int
+    quantidade:int
+    desconto:int=Field(default=0,ge=0,le=25) 
         
         
 class RelaTorioVendaSchema(ModelSchema):
